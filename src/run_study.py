033f9615023cb671e4d87ba0fc8be651558f2318
DIpna <sahanwijesi@gmail.com>
2026-07-19 20:35:23 +0530

    fix: validate study model selections

--- src/run_study.py
+++ src/run_study.py
@@ -5,13 +5,14 @@ import argparse
 
 DEFAULT_FRACTIONS = [0.01, 0.05, 0.1, 0.25, 0.5, 1.0]
 DEFAULT_SEEDS = [42, 123, 456]
+MODEL_NAMES = ["vit_scratch", "cnn", "vit_pretrained"]
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--fractions", nargs="+", type=float, default=DEFAULT_FRACTIONS)
     parser.add_argument("--seeds", nargs="+", type=int, default=DEFAULT_SEEDS)
-    parser.add_argument("--models", nargs="+", default=["vit_scratch", "cnn", "vit_pretrained"])
+    parser.add_argument("--models", nargs="+", choices=MODEL_NAMES, default=MODEL_NAMES)
     parser.add_argument("--epochs", type=int, default=15)
     parser.add_argument("--batch-size", type=int, default=64)
     parser.add_argument("--patience", type=int, default=3)
